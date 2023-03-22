15e1382e89f1c388b52fde5a7036fbc8e1bd7227
Xplosiveginger <vyshakvijay@gmail.com>
2023-03-22 18:08:27 +0530

    Updated Camera
    Third person camera functionality is setup.

--- Golf/Assets/Scripts/CameraAnchor.cs
+++ Golf/Assets/Scripts/CameraAnchor.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class CameraAnchor : MonoBehaviour
 {
     [SerializeField] private Transform golfBall;
+    [SerializeField] private Quaternion currentRotation;
+    [Range(1f, 10f)]
+    [SerializeField] private float sens = 1f;
+    [Range(6.1f, 30f)]
+    [SerializeField] private float zoomAmount = 6.13f;
+
+    private float sensitivity;
+    private float hRotation;
+    private float vRotation;
 
     // Update is called once per frame
     void Update()
     {
+        sensitivity = sens / 10f;
+
+        hRotation = Input.mousePosition.x;
+        vRotation = Input.mousePosition.y;
+
         transform.position = new Vector3(golfBall.position.x, golfBall.position.y - 0.25f, golfBall.position.z);
+
+        currentRotation.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + hRotation * sensitivity, transform.rotation.z + vRotation * -1 * sensitivity);
+        transform.localRotation = currentRotation;
     }
 }
